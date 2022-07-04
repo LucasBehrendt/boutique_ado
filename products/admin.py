ae121dd0a2edd476d4926673baeae26e9256692c
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-07-04 19:26:48 +0000

    Customize admin page

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Configuration for Category table on admin page"""
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Configuration for Product table on admin page"""
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+    ordering = ('sku', )
