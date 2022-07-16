fa7944dc8a5e0e6d50ba0399a57f634d3a9f248c
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-07-16 17:34:14 +0000

    Add loading overlay

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -50,10 +50,12 @@ card.addEventListener('change', function(event) {
 // Handle form submit
 let form = document.getElementById('payment-form');
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
     card.update({'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -65,9 +67,11 @@ form.addEventListener('submit', function(event) {
                 <span class="icon" role="alert">
                     <i class="fas fa-times"></i>
                 </span>
-                <span>${event.error.message}</span>
+                <span>${result.error.message}</span>
             `;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {
